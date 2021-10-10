464e67fde7f8a7205ff3fabab8994a81a326cb5f
EUNJEONGSONG <hellogine12@gmail.com>
2021-10-10 22:36:54 +0900

    modify Container class to Card class and ListTile class

--- lib/main.dart
+++ lib/main.dart
@@ -35,36 +35,28 @@ class MyApp extends StatelessWidget {
                     letterSpacing: 2.5,
                     color: Colors.teal.shade100,
                   )),
-              Container(
+              Card(
                 color: Colors.white,
-                padding: EdgeInsets.all(10.0),
                 margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                child: Row(children: <Widget>[
-                  Icon(Icons.phone, color: Colors.teal),
-                  SizedBox(
-                    width: 10.0,
-                  ),
-                  Text('+8210-9999-9999',
-                      style: TextStyle(
-                          color: Colors.teal.shade900,
-                          fontFamily: "Source Sans Pro",
-                          fontSize: 20.0))
-                ]),
-              ),
-              Container(
-                  color: Colors.white,
-                  padding: EdgeInsets.all(10.0),
-                  margin:
-                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
-                  child: Row(children: <Widget>[
-                    Icon(Icons.email, color: Colors.teal),
-                    SizedBox(width: 10.0),
-                    Text('test@gmail.com',
+                child: ListTile(
+                    leading: Icon(Icons.phone, color: Colors.teal),
+                    title: Text('+8210-9999-9999',
                         style: TextStyle(
                             color: Colors.teal.shade900,
                             fontFamily: "Source Sans Pro",
-                            fontSize: 20.0))
-                  ]))
+                            fontSize: 20.0))),
+              ),
+              Card(
+                  color: Colors.white,
+                  margin:
+                      EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                  child: ListTile(
+                      leading: Icon(Icons.email, color: Colors.teal),
+                      title: Text('test@gmail.com',
+                          style: TextStyle(
+                              color: Colors.teal.shade900,
+                              fontFamily: "Source Sans Pro",
+                              fontSize: 20.0))))
             ],
           )))),
     );
